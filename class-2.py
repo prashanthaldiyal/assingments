a5ad64fb1a67e969d24694e2edf8cf7e14e1dae7
prashant <prashant26haldiyal@gmail.com>
2018-10-02 17:38:03 +0530

    assingment on lecture 2

---
+++ class-2.py
@@ -0,0 +1,27 @@
+#question 1
+print("this is my first print function")
+
+#question 2
+a="space"
+b="time"
+print(a+b)
+
+#question 3
+print("enter 3 variables sperated by space:")
+x,y,z=map(int,input().split(" "))
+print("the values are:",x,y,z,end=" ")
+
+#question 4
+print("\nLet’s get started")
+
+#question 5
+s="Acadview" 
+course="Python" 
+fees=5000 
+print("%s offers %s course at %d rupees" %(s,course,fees))
+
+#question 6
+print("enter radius of a circle:",end="")
+r=int(input())
+pi=3.14
+print("\nthe area of circle is:",pi*r*r)
